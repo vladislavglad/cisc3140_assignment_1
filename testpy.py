03ea1368f678a4716665f08d953e3113d9c3ecaa
vladislavglad <53010628+vladislavglad@users.noreply.github.com>
2019-08-20 15:21:36 -0400

    Update testpy.py

--- testpy.py
+++ testpy.py
@@ -1,13 +1,1 @@
-from app import app
-import unittest
-
-#it is not Java; instead use print('hi')
-class testapp(unittest.TestCase):
-
-    def test_home(self):
-        test = app.test_client(self)
-        response = test.get('/')
-        self.assertEqual(response.status_code, 200)
-        
-if __name__ == '__main__':
-    unittest.main()
+print('Hello!')
